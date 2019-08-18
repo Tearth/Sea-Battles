db628c616044f92ea39ab4a007d99c7e1dd1d0d5
tearth <tearthdev@gmail.com>
2019-08-18 22:00:33 +0200

    Add script to improve ship building.

---
+++ SeaBattles/Assets/Scripts/ShipEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ShipEditorTools))]
+public class ShipEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ShipEditorTools script = (ShipEditorTools)target;
+        if (GUILayout.Button("Remove duplicates"))
+        {
+            script.RemoveDuplicates();
+        }
+
+        if (GUILayout.Button("Beautify names"))
+        {
+            script.BeautifyNames();
+        }
+
+        if (GUILayout.Button("Create mirror"))
+        {
+            script.CreateMirror();
+        }
+
+        if (GUILayout.Button("Remove mirror"))
+        {
+            script.RemoveMirror();
+        }
+    }
+}
---
+++ SeaBattles/Assets/Scripts/ShipEditorTools.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+
+public class ShipEditorTools : MonoBehaviour
+{
+    public Transform Blocks;
+
+    public void RemoveDuplicates()
+    {
+        var removedDuplicates = 0;
+
+        var children = GetAllBlocks();
+        var groups = children.GroupBy(p => p.transform.position);
+
+        foreach (var group in groups)
+        {
+            var blocks = group.ToList();
+            while (blocks.Count > 1)
+            {
+                DestroyImmediate(blocks[1]);
+                blocks.RemoveAt(1);
+
+                removedDuplicates++;
+            }
+        }
+
+        Debug.Log($"Removed duplicates: {removedDuplicates}");
+    }
+
+    public void BeautifyNames()
+    {
+        var blockId = 0;
+        foreach (Transform child in Blocks.transform)
+        {
+            child.gameObject.name = $"Block {blockId++}";
+        }
+
+        Debug.Log($"Beautified {blockId} blocks");
+    }
+
+    public void CreateMirror()
+    {
+        var blocksToCopy = Blocks.childCount;
+        var copiedBlocks = 0;
+
+        foreach (Transform child in Blocks.transform)
+        {
+            Instantiate(child.gameObject, Vector3.Scale(child.position, new Vector3(1, 1, -1)), Quaternion.identity, Blocks);
+
+            if (copiedBlocks++ >= blocksToCopy)
+            {
+                break;
+            }
+        }
+
+        Debug.Log($"Created new {copiedBlocks} blocks");
+    }
+
+    public void RemoveMirror()
+    {
+        var children = GetAllBlocks();
+        var removedBlocks = 0;
+        var blocksToRemove = children.Where(p => p.transform.position.z < 0).ToList();
+
+        foreach (var block in blocksToRemove)
+        {
+            DestroyImmediate(block);
+            removedBlocks++;
+        }
+
+        Debug.Log($"Removed {removedBlocks} blocks");
+    }
+
+    private List<GameObject> GetAllBlocks()
+    {
+        var children = new List<GameObject>();
+        foreach (Transform child in Blocks)
+        {
+            children.Add(child.gameObject);
+        }
+
+        return children;
+    }
+}
