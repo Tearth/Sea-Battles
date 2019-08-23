45f9f6ef4ed8f18bc0c9ea1a46cee77ddfcecb33
tearth <tearthdev@gmail.com>
2019-08-23 20:16:30 +0200

    Add multiple selection rectangle.

--- SeaBattles/Assets/Scripts/SelectionManager.cs
+++ SeaBattles/Assets/Scripts/SelectionManager.cs
@@ -7,8 +7,13 @@ public class SelectionManager : MonoBehaviour
     public Camera Camera;
     public GameObject SelectPrefab;
     public Transform Selections;
+    public int SelectionThickness;
+    public Color SelectionBckgColor;
+    public Color SelectionBorderColor;
 
     private GameObject _preSelect;
+    private Vector3 _mouseClickPoint;
+    private Texture2D _texture;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,10 @@ public class SelectionManager : MonoBehaviour
         _preSelect = Instantiate(SelectPrefab, Vector3.zero, Quaternion.identity, transform);
         _preSelect.GetComponent<SelectIndicatorEntity>().SetAsPreselect();
         _preSelect.SetActive(false);
+
+        _texture = new Texture2D(1, 1);
+        _texture.SetPixel(0, 0, Color.white);
+        _texture.Apply();
     }
 
     // Update is called once per frame
@@ -24,6 +33,11 @@ public class SelectionManager : MonoBehaviour
         if (!Cursor.visible)
         {
             return; 
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _mouseClickPoint = Input.mousePosition;
         }
 
         if (Physics.Raycast(Camera.ScreenPointToRay(Input.mousePosition), out var hit, float.MaxValue))
@@ -72,6 +86,43 @@ public class SelectionManager : MonoBehaviour
 
             HidePreselect();
         }
+    }
+
+    void OnGUI()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var minX = Mathf.Min(Input.mousePosition.x, _mouseClickPoint.x);
+            var maxX = Mathf.Max(Input.mousePosition.x, _mouseClickPoint.x);
+            var minY = Mathf.Min(Input.mousePosition.y, _mouseClickPoint.y);
+            var maxY = Mathf.Max(Input.mousePosition.y, _mouseClickPoint.y);
+
+            var rect = Rect.MinMaxRect(minX, minY, maxX, maxY);
+
+            // Background
+            DrawRectangle(rect.position, new Vector2(rect.size.x, -rect.size.y), SelectionBckgColor);
+
+            // Top
+            DrawRectangle(rect.position, new Vector2(rect.width, SelectionThickness), SelectionBorderColor);
+
+            // Bottom
+            DrawRectangle(rect.position + new Vector2(0, rect.height + SelectionThickness), new Vector2(rect.width, SelectionThickness), SelectionBorderColor);
+
+            // Left
+            DrawRectangle(rect.position, new Vector2(SelectionThickness, -rect.height), SelectionBorderColor);
+
+            // Right
+            DrawRectangle(rect.position + new Vector2(rect.width - SelectionThickness, 0), new Vector2(SelectionThickness, -rect.height), SelectionBorderColor);
+        }
+    }
+
+    private void DrawRectangle(Vector2 position, Vector2 size, Color color)
+    {
+        var invertedPosition = new Vector3(position.x, Camera.main.pixelHeight - position.y);
+
+        GUI.color = color;
+        GUI.DrawTexture(new Rect(invertedPosition, size), _texture);
+        GUI.color = Color.white;
     }
 
     private void ShowPreselect(Transform target)
